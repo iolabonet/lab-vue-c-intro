e9359b4a41ba7739058728bb70746e15b4742883
iolabonet <iolabonet.atg@gmail.com>
2023-03-24 15:23:00 +0100

    first commit

---
+++ Hello vue/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="">
+    </div>
+  </header>
+
+</template>
+
+
+<script setup>
+// import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+
+<style scoped>
+
+.logo {
+  display: block;
+}
+</style>
